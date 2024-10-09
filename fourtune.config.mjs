5bcd4a0a9b12563f581d7c230e920c362894764f
eXory2024 <exory@anio.software>
2024-10-09 22:40:52 +0200

    Reset fourtune config.

--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -1,22 +1,10 @@
-import {generateFromTemplate} from "fourtune/autogenerate"
-
-const asyncToSync = {
-	"import {scandir} from \"@anio-fs/scandir\"": "import {scandirSync} from \"@anio-fs/scandir\"",
-	"import {remove} from \"@anio-fs/remove\"": "import {removeSync} from \"@anio-fs/remove\"",
-	"export default async function(": "export default function(",
-	"filter = async (entry) => {": "filter = (entry) => {",
-	"const keep = await options.preserve(entry)": "const keep = options.preserve(entry)",
-	"await scandir": "scandirSync",
-	"async callback(entry) {": "callback(entry) {",
-	"await remove(": "removeSync("
-}
+import {generateSyncAsyncVariant} from "fourtune/autogenerate"
 
 export default {
 	realm: "js",
 	type: "package",
 
 	autogenerate: {
-		"sync.mjs": generateFromTemplate("src/template.mjs", asyncToSync),
-		"async.mjs": generateFromTemplate("src/template.mjs", {})
+
 	}
 }
