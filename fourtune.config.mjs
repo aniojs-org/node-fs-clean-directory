4a055a58168e6140cedd41fe47a8862efe223c04
eXory2024 <exory@anio.software>
2024-10-15 15:03:38 +0200

    Add package config.

--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -7,5 +7,14 @@ export default {
 	autogenerate: {
 		"export/CleanOptionsType.d.mts": generateSyncAsyncVariant("template/CleanOptionsType.d.mts", "async"),
 		"export/CleanSyncOptionsType.d.mts": generateSyncAsyncVariant("template/CleanOptionsType.d.mts", "sync"),
+	},
+
+	target: {
+		function_name: "clean",
+
+		dependencies: {
+			"@anio-fs/scandir": "scandir",
+			"@anio-fs/remove": "remove"
+		}
 	}
 }
