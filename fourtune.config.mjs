85cfbd661cd5c6d823fd8dd7740e76c952ee3dd3
eXory2024 <exory@anio.software>
2024-10-15 15:02:29 +0200

    Update project type.

--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -2,7 +2,7 @@ import {generateSyncAsyncVariant} from "fourtune/autogenerate"
 
 export default {
 	realm: "js",
-	type: "package",
+	type: "package:async/sync",
 
 	autogenerate: {
 		"export/CleanOptionsType.d.mts": generateSyncAsyncVariant("template/CleanOptionsType.d.mts", "async"),
