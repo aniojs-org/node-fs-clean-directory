5a94b36aa9d60fb4ec6b3dcc62495e0ebc7e740d
eXory2024 <exory@anio.software>
2024-10-15 15:01:58 +0200

    Remove obsolete auto generated files.

--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -7,11 +7,5 @@ export default {
 	autogenerate: {
 		"export/CleanOptionsType.d.mts": generateSyncAsyncVariant("template/CleanOptionsType.d.mts", "async"),
 		"export/CleanSyncOptionsType.d.mts": generateSyncAsyncVariant("template/CleanOptionsType.d.mts", "sync"),
-
-		"export/clean.mts": generateSyncAsyncVariant("template/clean.mts", "async"),
-		"export/cleanSync.mts": generateSyncAsyncVariant("template/clean.mts", "sync"),
-
-		"export/cleanFactory.mts": generateSyncAsyncVariant("template/cleanFactory.mts", "async"),
-		"export/cleanSyncFactory.mts": generateSyncAsyncVariant("template/cleanFactory.mts", "sync"),
 	}
 }
