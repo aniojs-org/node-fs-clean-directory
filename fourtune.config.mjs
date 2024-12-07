0c570ce5bbcec61a0409b8df9c3b8aec4f503a64
eXory2024 <exory@anio.software>
2024-12-08 00:22:05 +0100

    Update fourtune.config.mjs.

--- fourtune.config.mjs
+++ fourtune.config.mjs
@@ -1,20 +1,6 @@
-import {generateAsyncSyncVariant} from "fourtune/autogenerate"
-
 export default {
-	realm: "js",
-	type: "package:async/sync",
-
-	autogenerate: {
-		"src/export/CleanOptions.d.mts": generateAsyncSyncVariant("src/template/CleanOptions.d.mts", "async"),
-		"src/export/CleanSyncOptions.d.mts": generateAsyncSyncVariant("src/template/CleanOptions.d.mts", "sync"),
-	},
-
-	target: {
-		function_name: "clean",
-
-		dependencies: {
-			"@anio-fs/scandir": "scandir",
-			"@anio-fs/remove": "remove"
-		}
+	realm: {
+		name: "js",
+		type: "package"
 	}
 }
